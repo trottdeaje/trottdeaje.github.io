ecf2b0f2724bc6737210f69501aeb55e234f19f2
Aj <32769513+trottdeaje@users.noreply.github.com>
2020-04-22 22:28:57 -0300

    Updates

---
+++ precache-manifest.21e634619fce354081119f5f342f4f24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12843d7cc1f757e2511e15b911122931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814a82aaf7a71af2aa34",
+    "url": "/static/css/main.6e30a72a.chunk.css"
+  },
+  {
+    "revision": "418f2d5e30a49af8f187",
+    "url": "/static/js/2.5937081a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5937081a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814a82aaf7a71af2aa34",
+    "url": "/static/js/main.ad3fb909.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "431f0af815cac8df9b8c5a446969983b",
+    "url": "/static/media/ambersmith-thumb.431f0af8.png"
+  },
+  {
+    "revision": "142b7c744c28b432a12c1013cc2f707c",
+    "url": "/static/media/logo.142b7c74.svg"
+  },
+  {
+    "revision": "465499916b438a32dce910998472c284",
+    "url": "/static/media/oxapt-thumb.46549991.png"
+  },
+  {
+    "revision": "a0603947ab5fe5f0b78899a7cdf68381",
+    "url": "/static/media/vvs.a0603947.png"
+  }
+]);
